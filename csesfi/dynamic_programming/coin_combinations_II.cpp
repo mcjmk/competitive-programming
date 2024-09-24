34ef6d01c4d4a407fb6ad27b54ea41e300727a28
Maciej Mężyk <59283727+mcjmk@users.noreply.github.com>
2024-09-24 21:53:07 +0300

    Add files via upload

---
+++ csesfi/dynamic_programming/coin_combinations_II.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int MOD = 1e9+7;
+ 
+int main(){
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(0);
+ 
+	int n, x; 
+	cin>>n>>x;
+ 
+	vector<int>coins(n);
+	for (int i=0; i<n; i++)
+		cin>>coins[i];
+ 
+	vector<int>count(x+1);
+	count[0] = 1;
+
+	for (auto c : coins){
+		for (int i=1; i<=x; i++){
+			if (i-c >= 0 && count[i-c]) {
+				count[i] += count[i-c];
+				count[i] %= MOD;
+			}
+		}
+	}
+	cout<<count[x];
+}
